4e7e0a140034bea15e628f2e7854c44ca0a278d5
Mesi <mse212002@gmail.com>
2019-11-19 12:16:02 +0200

    Implemented basic functionality for the app  :up:

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log("Hi");
+let library = [];
+
+const Book = function(title, author, descript){
+  this.title = title;
+  this.author = author;
+  this.descript = descript;
+}
+
+function adBooks(book){
+  library.push(book);
+}
+
+let book1 = new Book("The Da Vinci Code", "Dan Brown", "Fiction book");
+let book2 =  new Book("Harry Potter", "J.K.Rowling", "Book for children");
+adBooks(book1);
+adBooks(book2);
+ 
+function render(){
+    let books = `<table><thead><tr><th>Author</th><th>Title</th><th>Description</th></tr></thead><tbody>`
+
+    library.forEach(({title, author, descript})  => {
+        books += `<tr><td>${title}</td> <td>${author}</td> <td>${descript}</td></tr>`;
+    })
+    books += `</tbody></table>`;
+    document.getElementById("content").innerHTML = books;
+}
+
+render();
