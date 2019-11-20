fbe092157cac15b3c362d308c92237907722aedd
imhta <imhtapm@gmail.com>
2019-11-20 22:16:01 +0530

    remove books from library and some fine tunning

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const closeSidebarBtn = document.getElementById('close-btn');
 const contentElement = document.getElementById('content');
 const formElement = document.getElementById('new-book-form');
 
-const library = JSON.parse(localStorage.getItem('library')) || [];
+const library =  getFromLocalStorage() || [];
 
 function Book([title, noOfPages, author, descript, status = 'not-read']) {
   this.title = title;
@@ -20,6 +20,12 @@ function addBooks(book) {
 
 function deleteBook(id) { 
   library.splice(id, 1); 
+}
+function getFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('library'));
+}
+function saveToLocalStorage() {
+  localStorage.setItem('library', JSON.stringify(library));
 }
 
 const toggleSidebar = () => {
@@ -51,12 +57,16 @@ function render() {
           <td>${title}, ${noOfPages} Pages</td>
           <td>${author}</td>
           <td>${descript}</td>
-          <td id='${i}' class='${status === 'not-read' ? 'not-read' : 'read'}'>
-          <i class='material-icons'>
+          <td id='${i}' data-click-type='status' class='${status === 'not-read' ? 'not-read' : 'read'}'>
+          <i data-click-type='status' class='material-icons'>
           ${status === 'not-read' ? 'maximize' : 'done'}
           </i>
           </td>
-          <td><button class='delete' onClick='deleteBook(${i-1})'>Delete</button></td>
+          <td id='${i}' data-click-type='delete' class='delete-btn'>
+            <i data-click-type='delete' class='material-icons'>
+              delete
+            </i>
+          </td>
         </tr>`;
       i += 1;
     });
@@ -86,27 +96,28 @@ formElement.addEventListener('submit', (e) => {
     data.push(inputData);
   });
   addBooks(new Book(data));
-  localStorage.setItem('library', JSON.stringify(library));
+  saveToLocalStorage();
   render();
   formElement.reset();
   toggleSidebar();
   e.preventDefault();
 });
 
-/* contentElement.addEventListener('click', (e) => {
+contentElement.addEventListener('click', (e) => {
+  const parentNodeId = e.target.parentNode.id
+  const clickType = e.target.getAttribute('data-click-type');
+  const hasBookId = parentNodeId !== '' && !isNaN(Number(parentNodeId));
   // eslint-disable-next-line no-restricted-globals
-  if (e.target.parentNode.id !== '' && !isNaN(Number(e.target.parentNode.id))) {
-    const bookId = Number(e.target.parentNode.id) - 1;
-    const currentState = library[bookId].status;
-    library[bookId].status = currentState === 'not-read' ? 'read' : 'not-read';
+  if (hasBookId) {
+    if(clickType === 'status') {
+      const bookId = Number(parentNodeId) - 1;
+      const currentState = library[bookId].status;
+      library[bookId].status = currentState === 'not-read' ? 'read' : 'not-read';
+    } else if (clickType === 'delete'){
+      deleteBook(Number(parentNodeId) - 1);
+      saveToLocalStorage();
+    }
     render();
   }
   e.preventDefault();
 });
-
-contentElement.addEventListener('click', (e) =>{
-  deleteBook(e.target.id);
-  localStorage.setItem('library', JSON.stringify(library));
-  render();
-  e.preventDefault();
-}); */
