c78d0b4e04b860a3f24d869a3c95c19158b29ef8
imhta <imhtapm@gmail.com>
2019-11-20 16:22:22 +0530

    single quotes lint fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const sidebarElement = document.getElementById("sidebar");
-const addNewBookBtn = document.getElementById("new-book-btn");
-const closeSidebarBtn = document.getElementById("close-btn");
-const saveNewBookBtn = document.getElementById("save-btn");
-const contentElement = document.getElementById("content");
-const formElement = document.getElementById("new-book-form");
+const sidebarElement = document.getElementById('sidebar');
+const addNewBookBtn = document.getElementById('new-book-btn');
+const closeSidebarBtn = document.getElementById('close-btn');
+const saveNewBookBtn = document.getElementById('save-btn');
+const contentElement = document.getElementById('content');
+const formElement = document.getElementById('new-book-form');
 
-let library = JSON.parse(localStorage.getItem("library")) || [];
+let library = JSON.parse(localStorage.getItem('library')) || [];
 
 const Book = function([title, author, descript, status = 'not read']) {
   this.title = title;
@@ -20,12 +20,12 @@ function addBooks(book) {
 
 const toggleSidebar = () =>
   (sidebarElement.style.display =
-    sidebarElement.style.display === "block" ? "none" : "block");
+    sidebarElement.style.display === 'block' ? 'none' : 'block');
 
 function render() {
   if (library.length > 0) {
     let books = `
-    <table id="table">
+    <table id='table'>
       <thead>
         <tr>
           <th>Id</th>
@@ -39,12 +39,12 @@ function render() {
     let i = 1;
     library.forEach(({ title, author, descript }) => {
       books += `
-        <tr id="row-${i}">
+        <tr id='row-${i}'>
           <td>${i}</td>
           <td>${title}</td>
           <td>${author}</td>
           <td>${descript}</td>
-          <td id="status-${i}" class="not-read"><i class="material-icons">
+          <td id='status-${i}' class='not-read'><i class='material-icons'>
           maximize
           </i></td>
         </tr>`;
@@ -62,40 +62,40 @@ function render() {
 
 render();
 
-addNewBookBtn.addEventListener("click", () => {
+addNewBookBtn.addEventListener('click', () => {
   toggleSidebar();
 });
 
-closeSidebarBtn.addEventListener("click", () => {
+closeSidebarBtn.addEventListener('click', () => {
   toggleSidebar();
 });
 
-formElement.addEventListener("submit", e => {
+formElement.addEventListener('submit', e => {
   const formData = new FormData(e.target);
   const data = [];
   formData.forEach(inputData => {
     data.push(inputData);
   });
   addBooks(new Book(data));
-  localStorage.setItem("library", JSON.stringify(library));
+  localStorage.setItem('library', JSON.stringify(library));
   render();
   formElement.reset();
   toggleSidebar();
   e.preventDefault();
 });
 
-contentElement.addEventListener("click", e => {
+contentElement.addEventListener('click', e => {
   document.getElementById(e.target.id).innerHTML =
-    document.getElementById(e.target.id).className === "not-read"
-      ? `<i class="material-icons">
+    document.getElementById(e.target.id).className === 'not-read'
+      ? `<i class='material-icons'>
       done
       </i>`
-      : `<i class="material-icons">
+      : `<i class='material-icons'>
       maximize
       </i>`;
   document.getElementById(e.target.id).className =
-    document.getElementById(e.target.id).className === "not-read"
-      ? "read"
-      : "not-read";
+    document.getElementById(e.target.id).className === 'not-read'
+      ? 'read'
+      : 'not-read';
   e.preventDefault();
 });
